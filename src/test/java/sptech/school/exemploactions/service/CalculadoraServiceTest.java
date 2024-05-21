3b220648b0e933a168d9614f5fb8a76ad40ea076
clarasalomaofaria <clara.faria@sptech>
2024-05-20 21:27:07 -0300

    Adicionando uma classe de testes para CalculadoraService

---
+++ src/test/java/sptech/school/exemploactions/service/CalculadoraServiceTest.java
@@ -0,0 +1,20 @@
+package sptech.school.exemploactions.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraServiceTest {
+
+
+    @Test
+    @DisplayName("Quando acionado com 10 e 2 deve retornar 5")
+    public void testDividir() {
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double resultado = calculadoraService.dividir(10, 2);
+        assertEquals(5, resultado);
+    }
+
+
+}
